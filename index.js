ff580b1d8458aaac228dd2c96016f067ad3d6c58
Mehmet Emin <c0594313@gmail.com>
2024-12-10 00:18:41 +0300

    Enable multiple users with individual theme colors

--- index.js
+++ index.js
@@ -27,31 +27,31 @@ function creationDate() {
 
 const app = express();
 const port = 3000;
+let currentUserID = 1;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
+  const response = await db.query("SELECT * FROM users");
+  const users = response.rows;
+  let query =
+    "select * from todos WHERE category = $1 AND user_id = $2 ORDER BY created_at ASC";
   try {
-    const today_todo = await db.query(
-      "select * from todos WHERE category = $1 ORDER BY created_at ASC",
-      ["today"]
-    );
+    const today_todo = await db.query(query, ["today", currentUserID]);
+    const week_todo = await db.query(query, ["week", currentUserID]);
+    const month_todo = await db.query(query, ["month", currentUserID]);
 
-    const week_todo = await db.query(
-      "select * from todos WHERE category = $1 ORDER BY created_at ASC",
-      ["week"]
-    );
-
-    const month_todo = await db.query(
-      "select * from todos WHERE category = $1 ORDER BY created_at ASC",
-      ["month"]
-    );
+    const currentUserColor = users.find(
+      (user) => user.id == currentUserID
+    ).color;
 
     res.render("index.ejs", {
+      currentUserColor,
       todayList: today_todo.rows,
       weekList: week_todo.rows,
       monthList: month_todo.rows,
+      users: users,
     });
   } catch (error) {
     console.log(error);
@@ -66,9 +66,10 @@ app.post("/add/:type", async (req, res) => {
   if (newItem) {
     try {
       const timeStamp = creationDate();
+      console.log("timeStamp: ", timeStamp);
       await db.query(
-        "INSERT INTO todos (title, category, created_at) VALUES ($1, $2, $3)",
-        [newItem, type, timeStamp]
+        "INSERT INTO todos (title, category, created_at, user_id) VALUES ($1, $2, $3, $4)",
+        [newItem, type, timeStamp, currentUserID]
       );
     } catch (error) {
       console.log(error);
@@ -105,6 +106,34 @@ app.post("/delete/:type", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+});
+
+app.get("/addUser", (req, res) => {
+  res.render("components/addMember.ejs");
+});
+
+app.post("/addUser", (req, res) => {
+  console.log("addUser route");
+  let { user, addNew } = req.body;
+  // Yeni bir kullanıcı eklemek istiyor aksi takdirde sadece currentUserID'yi degistirmek istiyor
+  if (addNew) {
+    return res.render("components/addMember.ejs");
+  }
+  if (!user || isNaN(user)) {
+    return res.status(400).send("Invalid user ID");
+  }
+  currentUserID = Number(user);
+  return res.redirect("/");
+});
+
+app.post("/login", async (req, res) => {
+  const { username, color } = req.body;
+  const response = await db.query(
+    "INSERT INTO users (username, color) VALUES ($1, $2) RETURNING id",
+    [username, color]
+  );
+  currentUserID = response.rows[0].id;
+  res.redirect("/");
 });
 
 app.listen(port, () => {
